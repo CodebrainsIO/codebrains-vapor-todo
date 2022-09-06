792c960fc3da33b017fdaa0ca6719dd95f23a76d
CodebrainsIO <coonce.james@codebrains.io>
2022-09-06 10:28:07 -0400

    Update Migration and controllers

--- Sources/App/Controllers/TodoController.swift
+++ Sources/App/Controllers/TodoController.swift
@@ -7,6 +7,8 @@ struct TodoController: RouteCollection {
         todos.get(use: index)
         todos.post(use: create)
         todos.group(":todoID") { todo in
+            todo.get(use: show)
+            todo.put(use: update)
             todo.delete(use: delete)
         }
     }
@@ -15,8 +17,28 @@ struct TodoController: RouteCollection {
         try await Todo.query(on: req.db).all()
     }
 
+    func show(req: Request) async throws -> Todo {
+        return try await Todo.find(req.parameters.get("todoID"), on: req.db)
+            .unwrap(or: Abort(.notFound)).get()
+    }
+
     func create(req: Request) async throws -> Todo {
         let todo = try req.content.decode(Todo.self)
+        try await todo.save(on: req.db)
+        return todo
+    }
+
+    func update(req: Request) async throws -> Todo {
+        guard let id = try await req.parameters.get("todoID", as: UUID.self) else {
+            throw Abort(.badRequest)
+        }
+
+        let updateTodo = try req.content.decode(Todo.self)
+        let todo = try await Todo.find(req.parameters.get("todoID"), on: req.db)
+            .unwrap(or: Abort(.notFound)).get()
+        todo.id = id
+        todo.title = updateTodo.title
+        todo.completed = updateTodo.completed
         try await todo.save(on: req.db)
         return todo
     }
--- Sources/App/Models/Todo.swift
+++ Sources/App/Models/Todo.swift
@@ -10,10 +10,14 @@ final class Todo: Model, Content {
     @Field(key: "title")
     var title: String
 
+    @Field(key: "completed")
+    var completed: Bool
+
     init() { }
 
-    init(id: UUID? = nil, title: String) {
+    init(id: UUID? = nil, title: String, completed: Bool) {
         self.id = id
         self.title = title
+        self.completed = completed
     }
 }
